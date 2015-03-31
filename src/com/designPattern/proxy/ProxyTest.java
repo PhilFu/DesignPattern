091d6da3644c0c9e2ff72ad3c755f0686e04a42f
PhilFu <mrfoxysky@gmail.com>
2015-03-31 22:51:03 +0800

    add comments to proxy pattern

--- src/com/designPattern/proxy/ProxyTest.java
+++ src/com/designPattern/proxy/ProxyTest.java
@@ -1,5 +1,14 @@
 package com.designPattern.proxy;
 
+/**
+ * Sometimes we cann't operate a object directly, but we need to interact with
+ * it, then we can use proxy to instead that object For example: a very big
+ * picture, it will cost a lot of time to open it within a document, we can use
+ * a proxy to instead it, and open the document immediately.
+ * 
+ * @author DeBen
+ *
+ */
 public class ProxyTest {
 
 	public static void main(String[] args) {
